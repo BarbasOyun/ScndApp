6c9a681aef7a6488b6bcb9a9da27a1fab2d7a66a
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-01-11 11:25:13 +0100

    Update

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
-console.log("Hello")
+//console.log("Hello")
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let x = 10;
+let y = 10;
+
+function gameLoop() {
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 ctx.fillStyle = 'red';
-ctx.fillRect(10, 10, 150, 100);
+ctx.fillRect(x, 10, 100, 100);
+
+x += 2;
+}
+
+setInterval(gameLoop, 1000 / 60)
