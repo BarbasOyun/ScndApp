5a91a5eef2a3fddb987a0a3c1176fb64fab52eac
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-01-11 12:22:32 +0100

    Moving square

--- game.js
+++ game.js
@@ -3,17 +3,37 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = document.documentElement.clientWidth || 
+document.body.clientWidth;
+canvas.height = document.documentElement.clientHeight || 
+document.body.clientHeight;
+
 let x = 10;
 let y = 10;
+
+let xDir = 5;
+let yDir = 5;
 
 function gameLoop() {
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.fillStyle = 'red';
-ctx.fillRect(x, 10, 100, 100);
+ctx.fillRect(x, y, 100, 100);
 
-x += 2;
+x += xDir;
+y += yDir;
+
+if (0 >= x || x + 100 >= canvas.width) 
+{
+    xDir *= -1;
+}
+
+if (0 >= y || y + 100 >= canvas.height)
+{
+    yDir *= -1;
+}
+
 }
 
 setInterval(gameLoop, 1000 / 60)
